9619474fc44244dee092a40600a39ffd7cc85bf1
MrWyss <marius.wyss@gmail.com>
2023-04-28 02:10:13 +0200

    adding version

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ from utils.autopilotutility import convert_from_autopilot_hash
 
 app = Flask(__name__)
 
+version = 'v1.0.2'
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         hash_input = request.form['hash']
         hash_list = convert_from_autopilot_hash(Hash=hash_input)
-        return render_template('home.html', result=hash_list)
+        return render_template('home.html', result=hash_list, version=version)
     else:
-        return render_template('home.html')
+        return render_template('home.html', version=version)
 
 if __name__ == '__main__':
     app.run(debug=True)
